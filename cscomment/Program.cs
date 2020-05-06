90671c0379799bebfd7d9b1df79d4d8c90a41626
hwon22 <s2018s06@e-mirim.hs.kr>
2020-05-06 15:15:19 +0900

    cscomment 실습 소스

--- cscomment/Program.cs
+++ cscomment/Program.cs
@@ -9,7 +9,25 @@ namespace cscomment
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            int x, y;
+            /*주석?*/
+            Console.Write("X 입력: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Y 입력: ");
+            y = int.Parse(Console.ReadLine());
+
+            if (x>4)
+            {
+                if (y > 2)
+                {
+                    Console.WriteLine(x*y);
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+            }
         }
     }
 }
